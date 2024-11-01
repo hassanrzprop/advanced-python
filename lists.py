0bba7a4ccd1f8962a9536ec71b95ba75571b7ed3
hassanrzprop <hassanrzprop@gamil.com>
2024-11-01 22:16:03 +0500

    lists

---
+++ lists.py
@@ -0,0 +1,24 @@
+fruits=['apple', 'orange','bannana',"gava",'grapes']
+# checking item included in it
+fruit="bannana"
+# if fruit in fruits:
+#     print("yes! removed")
+#     fruits.remove(fruit)
+#     print(fruits);
+# else:
+#     fruits.append(fruit)
+#     print("no! but added")
+# for i in fruits:
+#     print(i)
+
+# print(len(fruits))        
+
+
+fruits.insert(1,"Bluebarrey")
+print(fruits)
+
+
+# squaring every number in the list
+numList=[1,2,3,4,5,6,7,8,9]
+bList=[i*i for i in numList]
+print(bList)
